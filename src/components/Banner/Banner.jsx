e36db7d84fa9d3abff2cd91eff446741c34760de
Pritom <98pritom@gmail.com>
2025-03-30 14:57:20 +0600

    books-data-added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,18 +3,18 @@ import bannerImg from '../../assets/books.jpg';
 
 const Banner = () => {
     return (
-        <div className="hero bg-base-200 min-h-screen">
+        <div className="hero bg-base-200 min-h-[500px] px-4 sm:px-6 lg:px-8 rounded-2xl mt-5 mb-5">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <img
                     src={bannerImg}
-                    className="max-w-sm rounded-lg shadow-2xl" />
-                <div>
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
-                    <p className="py-6">
+                    className="w-full max-w-xs sm:max-w-sm lg:max-w-md rounded-lg shadow-2xl" />
+                <div className='text-center lg:text-left lg:ml-10'>
+                    <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold">Box Office News!</h1>
+                    <p className="py-4 sm:py-6 text-sm sm:text-base">
                         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
                         quasi. In deleniti eaque aut repudiandae et a id nisi.
                     </p>
-                    <button className="btn btn-primary">Get Started</button>
+                    <button className="btn btn-primary mt-2 sm:mt-4">Get Started</button>
                 </div>
             </div>
         </div>
--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,27 +1,37 @@
-import React from 'react';
+import { CiStar } from "react-icons/ci";
+import { Link } from "react-router-dom";
 
 const Book = ({ book }) => {
-    const { bookId, bookName, author, image } = book;
+    const { bookId, bookName, author, image, tags, category, rating } = book;
     return (
-        <div className="card bg-base-100 w-96 shadow-sm p-6 rounded-2xl">
-            <figure className='bg-gray-200 py-8 rounded-2xl'>
-                <img
-                    src={image}
-                    className='h-[166px]'
-                    alt={bookName} />
-            </figure>
-            <div className="card-body">
-                <h2 className="card-title">
-                    {bookName}
-                    <div className="badge badge-secondary">NEW</div>
-                </h2>
-                <p>By: {author}</p>
-                <div className="card-actions justify-end">
-                    <div className="badge badge-outline">Fashion</div>
-                    <div className="badge badge-outline">Products</div>
+        <Link to={`/books/${bookId}`}>
+            <div className="card bg-base-100 w-96 shadow-sm p-6 rounded-2xl">
+                <figure className='bg-gray-200 py-8 rounded-2xl'>
+                    <img
+                        src={image}
+                        className='h-[166px]'
+                        alt={bookName} />
+                </figure>
+                <div className="card-body">
+                    <div className='flex justify-center gap-1.5'>
+                        {
+                            tags.map((tag, idx) => <button key={idx} className="btn btn-xs bg-green-100 text-green-600 rounded-4xl border-0 p-3">{tag}</button>)
+                        }
+                    </div>
+
+                    <h2 className="card-title">
+                        {bookName}
+                        <div className="badge badge-secondary">NEW</div>
+                    </h2>
+                    <p>By: {author}</p>
+                    <div className='border-t-1 border-dashed mt-2 mb-2'></div>
+                    <div className="card-actions justify-between items-center">
+                        <div className="text-lg">{category}</div>
+                        <div className="flex justify-center items-center text-lg gap-2">{rating} <CiStar className="text-2xl" /></div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 };
 
