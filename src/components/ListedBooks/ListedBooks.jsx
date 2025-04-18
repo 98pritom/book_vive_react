7ac02b0bbc674f39c3eb64c213322599df774a63
Pritom <98pritom@gmail.com>
2025-04-18 21:43:46 +0600

    updated v2

--- src/components/ListedBooks/ListedBooks.jsx
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -4,6 +4,7 @@ const ListedBooks = () => {
     return (
         <div>
             <h2>Listed books</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero provident, voluptatibus consequuntur aliquid natus sed ipsum, adipisci aliquam nemo ea ratione harum, perspiciatis aut voluptatem laboriosam quo possimus aspernatur molestias?</p>
         </div>
     );
 };
